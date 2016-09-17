8540e2bae8ef09bfda6c5df34c1b0203a1506a5f
NJCTY <ctygood@vip.qq.com>
2016-09-17 09:40:16 +0800

    database created

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,19 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Db;
 class Index extends Controller
 {
-	//加载默认视图
-    public function index()
+	//公司页面
+    public function comp()
+    {
+    	$code = $_GET['comp'];
+    	$company = Db::table('company')->where('code',$code)->find();
+    	$this->assign("company",$company);
+		return $this->fetch();
+    }
+
+    public function doc()
     {
 		return $this->fetch();
     }
